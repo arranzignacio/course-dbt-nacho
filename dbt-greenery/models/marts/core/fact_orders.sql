9ff1163676ccc5e67528820c530a06c58c909973
arranzignacio <arranzignacio@gmail.com>
2021-11-26 13:40:08 +0000

    just keeping fact orders, not dim

--- dbt-greenery/models/marts/core/fact_orders.sql
+++ dbt-greenery/models/marts/core/fact_orders.sql
@@ -13,9 +13,14 @@ SELECT
   p.discount AS promo_code_discount,
   o.user_id,
   u.first_name AS user_first_name,
-  o.tracking_id
+  o.tracking_id,
+  os.sale_amount,
+  os.sale_amount*(100-p.discount)/100 AS revenue,
+  os.sale_amount*(100-p.discount)/100 - order_total AS margin
 FROM {{ ref('stg_orders') }} o
 LEFT JOIN {{ ref('stg_promos') }} p
   ON o.promo_id = p.promo_id
 LEFT JOIN {{ ref('stg_users') }} u
   ON o.user_id = u.user_id
+LEFT JOIN {{ ref('int_order_sales') }} os
+  ON o.order_id = os.order_id
