326502369f840af20ace95be6818fd1131fa1e4d
arranzignacio <arranzignacio@gmail.com>
2021-11-27 11:55:57 +0000

    small changes

--- dbt-greenery/models/marts/core/fact_orders.sql
+++ dbt-greenery/models/marts/core/fact_orders.sql
@@ -15,8 +15,7 @@ SELECT
   u.first_name AS user_first_name,
   o.tracking_id,
   os.sale_amount,
-  os.sale_amount*(100-p.discount)/100 AS revenue,
-  os.sale_amount*(100-p.discount)/100 - order_total AS margin
+  o.order_total - o.order_cost AS margin
 FROM {{ ref('stg_orders') }} o
 LEFT JOIN {{ ref('stg_promos') }} p
   ON o.promo_id = p.promo_id
